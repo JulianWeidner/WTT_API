11ab4f92d27a65b10633210f808590bd44a3b864
Julian <julian.nicholas.weidner@gmail.com>
2024-07-03 19:00:26 +0200

    working api endpoint

--- api/views.py
+++ api/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+from .models import Tournament
+from .serializers import TournamentSerializer
+
+class TournamentViewSet(viewsets.ModelViewSet):
+    queryset = Tournament.objects.all()
+    serializer_class = TournamentSerializer
